63fde0a4ae522900c8d1135d32c4c867a7b541b1
velvetbaldmime <keynmol@gmail.com>
2014-07-28 21:27:35 +0100

    account for new functionality added to the server

--- agent.rb
+++ agent.rb
@@ -3,8 +3,13 @@ require 'faraday'
 require 'yaml'
 
 config = YAML.load_file('config.yml')
-connection=Faraday.new("http://#{config['empirist_server']['host']}:#{config['empirist_server']['port']}")
+connection=Faraday.new("http://#{config['empirist_server']['host']}:#{config['empirist_server']['port']}") do |f|
+								  f.request :multipart
+								  f.request :url_encoded
+								  f.adapter :net_http # This is what ended up making it work
+								end
 
+Dir.mkdir(config['cache_folder']) unless File.exists?(config['cache_folder'])
 
 get '/server' do
 	connection.build_url.to_s
@@ -12,6 +17,40 @@ end
 
 get '/local_cache' do
 	config["cache_folder"]
+end
+
+get '/find_trial/*' do
+	base_url=params["splat"][0]
+	redirect connection.build_url("find_trial/#{base_url}", params.reject{|k,v| ["splat","captures"].include? k})
+end
+
+get '/plot/*' do
+	base_url=params["splat"][0]
+	redirect connection.build_url("plot/#{base_url}", params.reject{|k,v| ["splat","captures"].include? k})
+end
+
+get '/annotation/:id' do
+	redirect connection.build_url("annotation/#{params['id']}")
+end
+
+
+get '/synchronise_plot' do
+	trial_id=params["id"]
+	plot_name=params["plot"]
+
+	plotfile=File.join config["cache_folder"], "#{trial_id}-#{plot_name}.pdf"
+	
+	if File.exists?(plotfile)
+					puts "uploading path: #{plotfile}, filesize: #{File.size(plotfile)}" 
+					
+					params={file: Faraday::UploadIO.new(plotfile, 'application/pdf'),
+							plot: plot_name,
+							trial_id: trial_id}
+
+					
+					connection.post '/upload_plot', params
+	end
+	
 end
 
 get '/dataset/*' do
